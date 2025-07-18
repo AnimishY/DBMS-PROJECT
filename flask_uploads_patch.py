730ad1b55757f130111a519f9f9bbd2c314d0ea6
Sanskriti23487 <163025975+Sanskriti23487@users.noreply.github.com>
2025-07-18 14:23:12 +0530

    Delete flask_uploads_patch.py

--- flask_uploads_patch.py
+++
@@ -1,7 +0,0 @@
-# flask_uploads_patch.py
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage
-
-def configure_uploads(app, upload_sets):
-    for name, upload_set in upload_sets.items():
-        upload_set._config(app, name)
